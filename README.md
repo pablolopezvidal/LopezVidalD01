bd03fc7c12a03a1358ab4723b021ba837cad7a30
Pablo Lopez Vidal <lopezvidalpablo6@gmail.com>
2024-01-07 22:10:30 +0100

    actualizacion readme

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 
 Contenido del ejercicio 1.
 
-![Ejemplo de imagen](imagenes/foto1.png)
+![Ejemplo de imagen](imagenes/foto4.png)
 
 # Ejercicio 2
 
 Contenido del ejercicio 2.
 
-![Ejemplo de imagen](imagenes/foto2.png)
+![Ejemplo de imagen](imagenes/foto5.png)
 
 
 # main.dart EXPLICACION:
@@ -200,4 +200,38 @@ Este método es obligatorio en un widget Flutter y se encarga de retornar la jer
   - **Ícono:** Se utiliza el ícono de recarga (`Icons.refresh`) con un tamaño de 30 unidades y color blanco.
 
 En resumen, `MyWidget4` representa una interfaz con un fondo azul, un número grande ('13') y botones redondos con funcionalidades específicas: aumentar, disminuir y recargar. Los estilos y tamaños están diseñados para crear una apariencia atractiva y fácil de usar.
+
+# Contador 
+
+fotos del ejercicio
+
+![Ejemplo de imagen](./videoReadme/foto1.png)
+![Ejemplo de imagen](./videoReadme/foto2.png)
+![Ejemplo de imagen](./videoReadme/foto3.png)
+
+## Estructura del Código
+
+El código está dividido en dos partes: la definición del widget principal y las funciones relacionadas.
+
+### Widget Principal
+
+El widget principal se llama `MyWidget4`, que es un StatefulWidget. Aquí se inicializa un contador y se construye la interfaz de usuario.
+
+- **Estado del Widget (`_MyWidget4State`):** Gestiona el estado del widget, incluyendo el valor del contador.
+
+- **Método `build`:** Construye la interfaz de usuario utilizando un contenedor con un color de fondo, un texto grande que muestra el valor actual del contador y tres botones distribuidos en una fila.
+
+- **Métodos `sumar`, `restar`, y `empezar`:** Actualizan el estado del contador según sea necesario.
+
+- **Método `buildRoundButton`:** Genera un botón redondo con un texto y una función de devolución de llamada asociada.
+
+- **Método `buildReloadButton`:** Genera un botón redondo con un ícono de recarga para reiniciar el contador.
+
+### Estilo y Diseño
+
+- **Color de Fondo:** Se utiliza un color azul claro para el fondo del contenedor.
+
+- **Fuente y Estilo de Texto:** La fuente utilizada es 'Poppins' de Google Fonts. El texto del contador es grande y blanco.
+
+- **Botones:** Los botones son redondos, con un color de fondo y borde específicos. Los botones de incremento y decremento ejecutan las funciones asociadas (`sumar` y `restar`), mientras que el botón de recarga ejecuta la función `empezar`.
 
--- lib/widgets/widget3.dart
+++ lib/widgets/widget3.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class MyWidget4 extends StatelessWidget {
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyWidget4 extends StatefulWidget {
+  @override
+  State<MyWidget4> createState() => _MyWidget4State();
+}
+
+class _MyWidget4State extends State<MyWidget4> {
+  int contador = 0; // Inicializa el contador con un valor inicial
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Color.fromARGB(255, 104, 183, 247),      
+      color: Color.fromARGB(255, 104, 183, 247),
       width: MediaQuery.of(context).size.width,
-      height:MediaQuery.of(context).size.height,
+      height: MediaQuery.of(context).size.height,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
@@ -15,8 +26,14 @@ class MyWidget4 extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
-                '13',
-                 style: GoogleFonts.poppins(textStyle: TextStyle(fontSize: 330,fontWeight: FontWeight.w100,color: Colors.white),),
+                '$contador',
+                style: GoogleFonts.poppins(
+                  textStyle: TextStyle(
+                    fontSize: 330,
+                    fontWeight: FontWeight.w100,
+                    color: Colors.white,
+                  ),
+                ),
               ),
             ],
           ),
@@ -24,11 +41,11 @@ class MyWidget4 extends StatelessWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              buildRoundButton('-1'),
-              SizedBox(width: 55), 
-              buildReloadButton(),
-              SizedBox(width: 55), 
-              buildRoundButton('+1'),
+              buildRoundButton('-1', restar),
+              SizedBox(width: 55),
+              buildReloadButton(empezar),
+              SizedBox(width: 55),
+              buildRoundButton('+1', sumar),
             ],
           ),
         ],
@@ -36,30 +53,59 @@ class MyWidget4 extends StatelessWidget {
     );
   }
 
-  Widget buildRoundButton(String text) {
-    return Container(
-      width: 60,
-      height: 60,
-      margin: EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        color: Color.fromARGB(255, 152, 212, 255),
-        borderRadius: BorderRadius.circular(30),
-        border: Border.all(
-          color: Colors.white,
-          width: 2.0,
+  void sumar() {
+    setState(() {
+      contador++;
+    });
+  }
+
+  void restar() {
+    setState(() {
+      if(contador>0){
+        contador--;
+      }
+    });
+  }
+  void empezar() {
+  setState(() {
+    contador = 0;
+  });
+}
+
+  }
+
+  Widget buildRoundButton(String text, VoidCallback pressBoton) {
+    return GestureDetector(
+      onTap: pressBoton,
+      child: Container(
+        width: 60,
+        height: 60,
+        margin: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Color.fromARGB(255, 152, 212, 255),
+          borderRadius: BorderRadius.circular(30),
+          border: Border.all(
+            color: Colors.white,
+            width: 2.0,
+          ),
         ),
-      ),
-      child: Center(
-        child: Text(
-          text,
-          style: TextStyle(fontSize: 20, color: Color.fromARGB(255, 255, 255, 255)), // Color azul
+        child: Center(
+          child: Text(
+            text,
+            style: TextStyle(
+              fontSize: 20,
+              color: Color.fromARGB(255, 255, 255, 255),
+            ),
+          ),
         ),
       ),
     );
   }
 
-  Widget buildReloadButton() {
-    return Container(
+Widget buildReloadButton(VoidCallback pressBoton) {
+  return GestureDetector(
+    onTap: pressBoton,
+    child: Container(
       width: 60,
       height: 60,
       margin: EdgeInsets.all(10),
@@ -72,8 +118,12 @@ class MyWidget4 extends StatelessWidget {
         ),
       ),
       child: Center(
-        child: Icon(Icons.refresh, size: 30, color: Color.fromARGB(255, 255, 255, 255)), // Color azul
+        child: Icon(
+          Icons.refresh,
+          size: 30,
+          color: Color.fromARGB(255, 255, 255, 255),
+        ), // Color azul
       ),
-    );
-  }
+    ),
+  );
 }
